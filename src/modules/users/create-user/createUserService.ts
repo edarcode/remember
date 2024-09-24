f20ef12aedea51659bbd0e90d8dc41b03b583d7d
Edwin Ortiz <edarcode@gmail.com>
2024-09-24 01:14:57 -0500

    ruta para crear usuario funcional

--- src/modules/users/create-user/createUserService.ts
+++ src/modules/users/create-user/createUserService.ts
@@ -2,9 +2,16 @@ import { z } from "zod";
 import { createUserSchema } from "./createUserSchema";
 import { db } from "../../../db/db";
 import { users } from "../../../db/schema";
+import bcrypt from "bcrypt";
+import { BCRYPT } from "../../../config/bcrypt";
 
 export const createUserService = async (newUser: User) => {
-  await db.insert(users).values({ ...newUser });
+  await db
+    .insert(users)
+    .values({
+      ...newUser,
+      password: await bcrypt.hash(newUser.password, BCRYPT.salt),
+    });
 };
 
 type User = z.infer<typeof createUserSchema>;
