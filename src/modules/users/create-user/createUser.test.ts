ff34cc18fc9ec8dd33ceb0df8b1b304811c6b1d0
Edwin Ortiz <edarcode@gmail.com>
2024-09-24 19:51:01 -0500

    refactor

--- src/modules/users/create-user/createUser.test.ts
+++ src/modules/users/create-user/createUser.test.ts
@@ -2,8 +2,22 @@ import req from "supertest";
 import server from "../../../server/server";
 
 describe("POST /users/create-user", () => {
-  test("should resp with status 400", async () => {
-    const res = await req(server).post("/users/create-user").send();
-    expect(res.status).toBe(400);
+  it("Debería responder con status 400 si los datos son invalidos", async () => {
+    const newUser = {
+      email: "",
+      password: "",
+    };
+    const res = await req(server).post("/users/create-user").send(newUser);
+    expect(res.statusCode).toBe(400);
   });
+
+  // it("Debería crear un nuevo usuario si los datos son válidos", async () => {
+  //   const newUser = {
+  //     email: "doe@example.com",
+  //     password: "pass123",
+  //   };
+
+  //   const res = await req(server).post("/users/create-user").send(newUser);
+  //   expect(res.statusCode).toBe(201);
+  // });
 });
